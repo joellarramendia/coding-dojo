31aefe3e2da40111efe304b349ea62225d5a994e
Joel <joellarramendia@gmail.com>
2024-03-12 10:41:27 -0300

    prueba react

---
+++ pruba_react/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TaskList from './components/TaskList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
---
+++ pruba_react/src/components/TaskList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TaskList() {
+  return (
+    <div>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default TaskList;
