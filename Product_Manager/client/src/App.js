e38a9fa09f275000221e3293f126aebb29da3739
Joel <joellarramendia@gmail.com>
2024-04-02 11:00:43 -0400

    .

--- Product_Manager/client/src/App.js
+++ Product_Manager/client/src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Main from "./views/Main";
 import Details from "./views/Details";
+import Update from "./views/Update";
 import './App.css';
+
+
 
 function App() {
   return (
@@ -10,6 +13,7 @@ function App() {
       <BrowserRouter>
         <Route exact path="/product" render={(routeProps) => <Main {...routeProps} />} />
         <Route path="/product/:id" render={(routeProps) => <Details {...routeProps} />} />
+        <Route path="/product/:id/edit" render={(routeProps) => <Update {...routeProps} />} />
       </BrowserRouter>
     </div>
   );
