48e25acac631e2de3fdedbde9e8c75852f0d4fdb
Joel <joellarramendia@gmail.com>
2024-04-02 10:56:17 -0400

    se agrego los modulos de actualizar y eliminar

--- Product_Manager/server/controllers/product.controller.js
+++ Product_Manager/server/controllers/product.controller.js
@@ -31,3 +31,33 @@ module.exports.getProduct = (req, res) => {
             res.status(500).json({ error: 'Internal Server Error' });
         });
 }
+
+module.exports.updateProduct = (req, res) => {
+    Product.findOneAndUpdate({_id: req.params.id}, req.body, {new: true})
+    .then(updateProduct => res.json(updateProduct))
+    .catch(err => {
+        console.error('Error updating product:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    });
+}
+
+/*exports.deleteProduct = async (req, res) => {
+    try {
+        const productId = req.params.id;
+        // Lógica para eliminar el producto de la base de datos
+        await Product.findByIdAndDelete(productId);
+        res.status(200).json({ message: 'Product deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting productsss:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};*/
+
+module.exports.deleteProduct = (req, res) => {
+    Product.deleteOne({_id: req.params.id})
+        .then(deleteConfirmation => res.json(deleteConfirmation))
+        .catch(err => {
+            console.error('Error deleting product:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        });
+}
